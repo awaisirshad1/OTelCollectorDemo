716435a9f951cbe7b7c48e3b8187b63bf6339b09
awaisirshad1 <awaisirshad1@icloud.com>
2025-05-09 15:37:02 -0400

    Project setup compete

---
+++ OTelCollectorDemo/src/main/java/opentelemetry/demo/otelcollectordemo/OTelCollectorDemoApplication.java
@@ -0,0 +1,14 @@
+package opentelemetry.demo.otelcollectordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class OTelCollectorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OTelCollectorDemoApplication.class, args);
+	}
+
+}
---
+++ OTelCollectorDemo/src/test/java/opentelemetry/demo/otelcollectordemo/OTelCollectorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package opentelemetry.demo.otelcollectordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OTelCollectorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
